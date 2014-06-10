ded9b26d7396ab7ae8e7dff2863ac1e93893219c
Qiaodan Jin Stone <qiaodan.jin.stone@gmail.com>
2014-06-10 00:37:35 -0400

    stuff for showing DMA packets in python

---
+++ sw/DMA_usb.py
@@ -0,0 +1,40 @@
+#!/usr/local/opt/python/bin/python2.7
+
+import serial, sys
+import math
+import numpy
+import matplotlib.pyplot as plt
+import csv
+import matplotlib.patches as mpatches
+
+DMA_l = (20)/2
+frame_size = 62*DMA_l;
+bytes_per_sample = 2;
+
+if __name__ == "__main__":
+    frames = [];
+
+    usbfriend = serial.Serial(sys.argv[1]);
+    
+    for frame_idx in xrange(1):
+        data = usbfriend.read(frame_size * bytes_per_sample);
+
+        frame = [];
+
+        for byte_idx in xrange(0, len(data), 2):
+            lsb = ord(data[byte_idx]);
+            msb = ord(data[byte_idx+1]);
+            val = (msb << 8) | lsb;
+            # TODO: add two's complement logic
+            frame.append(val);
+        
+        frames.append(frame);
+    
+    usbfriend.close();
+    
+    print frames
+    
+    plt.xlabel("Samples");
+    plt.ylabel("Signal");
+    plt.plot(frames[0]);
+    plt.show();
